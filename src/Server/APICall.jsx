5390e0dabe7d5305bc8b3f8be8c924b3e7e64f08
Shahrom <mshahromali@gmail.com>
2022-12-16 13:57:23 -0500

    Changing the return value to just the data
    Instead of returning the whole response, only returning the data from the response

--- src/Server/APICall.jsx
+++ src/Server/APICall.jsx
@@ -25,12 +25,12 @@ function APICall(_url, _method, _data) {
   }
 
   axios(request).then((res) => {
-    console.log(res);
+    console.log(res.data);
     console.log(res.statusText);
     if (res.status != 200) {
       return false;
     }
-    return res;
+    return res.data;
   });
 }
 
