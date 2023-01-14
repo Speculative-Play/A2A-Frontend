02b7121ed3cab5b0cc1ae4852f82cbe0570618c6
Shahrom <mshahromali@gmail.com>
2023-01-13 23:04:23 -0500

    Configuring Redux
    Incomplete. Find here: https://react-redux.js.org/tutorials/quick-start

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 //Packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './Server/store'
+import { Provider } from 'react-redux'
 
 //Components
 import App from './App';
 const root = document.getElementById('root');
 
-ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>, root);
+ReactDOM.render(<React.StrictMode> <Provider store={store}> <App /> </Provider> </React.StrictMode>, root);
